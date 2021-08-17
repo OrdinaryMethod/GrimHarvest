4ce7c7f1e7922b5a27944b95cf02d415363d2380
Cayden O'Shea <67022300+OrdinaryMethod@users.noreply.github.com>
2021-08-16 19:13:37 -0500

    Unrelated
    Player object will flip on axis for facing direction

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -15,11 +15,14 @@ public class PlayerMovement : MonoBehaviour
     private float playerSpeed;
     private float moveHorizontal;
 
+    [HideInInspector] public bool facingRight;
+
     [Header("Movement Conditions")]
     public bool isGrounded;
     public bool grabbingLedge;
     public bool canMove;
     public bool canClimb;
+
     
 
     //Keybinds
@@ -49,6 +52,26 @@ public class PlayerMovement : MonoBehaviour
         Move();
         jump();
         FixTheBugs();
+
+
+        //Player Movement. Check for horizontal movement
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0.5f && !facingRight)
+            {
+                //If we're moving right but not facing right, flip the sprite and set     facingRight to true.
+                Flip();
+                facingRight = true;
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0.5f && facingRight)
+            {
+                //If we're moving left but not facing left, flip the sprite and set facingRight to false.
+                Flip();
+                facingRight = false;
+            }
+
+        }
+
     }
 
     private void GetKeyBinds()
@@ -101,6 +124,17 @@ public class PlayerMovement : MonoBehaviour
         {
             isGrounded = false;
         }
+    }
+
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
     private void FixTheBugs()
