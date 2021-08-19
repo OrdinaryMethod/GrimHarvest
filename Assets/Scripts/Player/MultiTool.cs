6f9785e0f04a7e572d839e22ef2899155dce6535
Cayden O'Shea <67022300+OrdinaryMethod@users.noreply.github.com>
2021-08-18 19:01:19 -0500

    Aiming
    Basic aiming setup

---
+++ Assets/Scripts/Player/MultiTool.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiTool : MonoBehaviour
+{
+    //Variables
+    private Vector2 direction;
+
+    public float aimSpeed = 1000f; //Aim speed
+    [HideInInspector] public bool facingRight;
+
+    [Header("Aim Angles")]
+    private float setAngle = 0;
+    private float angle; //Global aim angle
+
+    [SerializeField] private float setAngleUp;
+    private float angleUp;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set values
+        angleUp = setAngleUp;
+        angle = setAngle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AimDirection();
+    }
+
+    private void AimDirection()
+    {    
+        //Player Movement. Check for horizontal movement
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {    
+            if (Input.GetAxisRaw("Horizontal") > 0.5f && !facingRight)
+            {
+                //If we're moving right but not facing right, flip the sprite and set     facingRight to true.
+                Flip();
+                facingRight = true;
+
+                //Flip aim angles
+                angleUp = setAngleUp;
+                angle = setAngle;
+
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0.5f && facingRight)
+            {
+                //If we're moving left but not facing left, flip the sprite and set facingRight to false.
+                Flip();
+                facingRight = false;
+
+                //Flip aim angles
+                angleUp = setAngleUp + 90;
+                angle = setAngle + 180;
+                
+            }
+
+        }
+        
+        if(Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("test");
+            angle = angleUp;
+            
+        }
+        else
+        {
+            if(facingRight)
+            {
+                angle = 0;
+            }
+            else
+            {
+                angle = 180;
+            }
+            
+        }
+
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, aimSpeed * Time.deltaTime);
+    }
+
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
