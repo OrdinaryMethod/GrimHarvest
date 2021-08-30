ca6b09773a6a6e0dd2bf105fa24fb02aa09b550b
Cayden O'Shea <67022300+OrdinaryMethod@users.noreply.github.com>
2021-08-30 18:17:22 -0500

    Update
    Sprite will flip based on direction
    Data extractor will stop on impact

--- Assets/Scripts/Player/Tools/DataExtractor.cs
+++ Assets/Scripts/Player/Tools/DataExtractor.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class DataExtractor : MonoBehaviour
 {
-    
+    //Variables
+    private bool facingRight;
+
+    void Start()
+    {
+        flip();
+    }
+
+    private void flip()
+    {
+        facingRight = GameObject.Find("Player").GetComponent<PlayerMovement>().facingRight;
+
+        if (!facingRight)
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+    }
 }
