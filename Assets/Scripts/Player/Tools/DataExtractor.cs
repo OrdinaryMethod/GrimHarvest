7b95d0ec623ab6bc3c9ca4cc264248c725a1836f
Cayden O'Shea <67022300+OrdinaryMethod@users.noreply.github.com>
2021-08-31 18:45:04 -0500

    Update
    Data extractor can now actually gain data

--- Assets/Scripts/Player/Tools/DataExtractor.cs
+++ Assets/Scripts/Player/Tools/DataExtractor.cs
@@ -8,6 +8,9 @@ public class DataExtractor : MonoBehaviour
     private Rigidbody2D rb2d;
 
     private bool facingRight;
+    private bool dataExtractionComplete;
+
+    public float dataExtractionTime;
 
     void Start()
     {
@@ -31,12 +34,24 @@ public class DataExtractor : MonoBehaviour
     {
         rb2d.velocity = new Vector2(0, 0);
         rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
+        StartCoroutine(ExtractTheData());
 
         //Recollect
         if(collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponentInChildren<MultiTool>().dataExtractorAmmo++;
-            Destroy(gameObject);
+            if(dataExtractionComplete)
+            {
+                collision.gameObject.GetComponentInChildren<MultiTool>().dataExtractorAmmo++;
+                Destroy(gameObject);
+            }          
         }
+    }
+
+    IEnumerator ExtractTheData()
+    {
+        Debug.Log("Extracting data...");
+        yield return new WaitForSeconds(dataExtractionTime);
+        dataExtractionComplete = true;
+        Debug.Log("Data gained");
     }
 }
