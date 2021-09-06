b126a2b4aff9b3e08236900a5c9222f834dc01d5
Cayden O'Shea <67022300+OrdinaryMethod@users.noreply.github.com>
2021-09-06 13:14:36 -0500

    animation
    animator conditions for running/idle setup

--- Assets/Scripts/Player/PlayerAnimController.cs
+++ Assets/Scripts/Player/PlayerAnimController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerAnimController : MonoBehaviour
 {
+    private Animator playerAnim;
+
+    [Header("Animation Conditions")]
+    public bool isRunning;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckKeys();
+    }
+
+    private void CheckKeys()
+    {
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            playerAnim.SetBool("running", true);
+        }
+        else
+        {
+            playerAnim.SetBool("running", false);
+        }
     }
 }
