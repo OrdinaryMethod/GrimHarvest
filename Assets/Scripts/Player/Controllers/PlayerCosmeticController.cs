54ed9faeb5f53a80595c36c9568aea8dcba13445
Cayden O'Shea <67022300+OrdinaryMethod@users.noreply.github.com>
2022-02-02 13:23:26 -0600

    cosmetic
    player will now go dark when hiding

--- Assets/Scripts/Player/Controllers/PlayerCosmeticController.cs
+++ Assets/Scripts/Player/Controllers/PlayerCosmeticController.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class PlayerCosmeticController : MonoBehaviour
 {
-    private GameObject[] playerArmor; 
+    private PlayerController _playerController;
+
+    [SerializeField] private GameObject[] _playerArmor; 
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _playerController = GetComponent<PlayerController>();
+        _playerArmor = GameObject.FindGameObjectsWithTag("PlayerArmor");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_playerController.isHidden)
+        {
+            for(int i = 0; i < _playerArmor.Length; i++)
+            {
+                _playerArmor[i].GetComponent<SpriteRenderer>().color = Color.black;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < _playerArmor.Length; i++)
+            {
+                _playerArmor[i].GetComponent<SpriteRenderer>().color = Color.white;
+            }
+        }
     }
 }
