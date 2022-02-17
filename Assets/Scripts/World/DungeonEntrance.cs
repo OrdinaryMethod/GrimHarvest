b25c6cf33e9494e65c52addbcccd73552d7d5404
Cayden O'Shea <67022300+OrdinaryMethod@users.noreply.github.com>
2022-02-16 20:52:55 -0600

    Update
    Foundation complete

--- Assets/Scripts/World/DungeonEntrance.cs
+++ Assets/Scripts/World/DungeonEntrance.cs
@@ -13,19 +13,29 @@ public class DungeonEntrance : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        dungeonMasterData.dungeonStarted = true;
-        dungeonMasterData.dungeonName = _dungeonName;
-        dungeonMasterData.currentLevel = 1;
-        dungeonMasterData.maxLevel = _dungeonScenes.Count;
-
-        for (int i = 0; i < _dungeonScenes.Count; i++)
+        if(!dungeonMasterData.dungeonStarted)
         {
-            dungeonMasterData.dungeonScenes.Add(_dungeonScenes[i]);
-        }
+            dungeonMasterData.dungeonStarted = true;
+            dungeonMasterData.dungeonName = _dungeonName;
+            dungeonMasterData.currentLevel = 1;
+            dungeonMasterData.maxLevel = _dungeonScenes.Count;
 
-        //Scene select
-        int sceneSelect = Random.Range(0, _dungeonScenes.Count);
-        SceneManager.LoadScene(_dungeonScenes[sceneSelect]);
+            for (int i = 0; i < _dungeonScenes.Count; i++)
+            {
+                dungeonMasterData.dungeonScenes.Add(_dungeonScenes[i]);
+            }
+
+            //Scene select
+            int sceneSelect = Random.Range(0, _dungeonScenes.Count);
+            SceneManager.LoadScene(_dungeonScenes[sceneSelect]);
+        }
+        else
+        {
+            //Choose a level based off of the data stored in the dungeon master object
+            int sceneSelect = Random.Range(0, _dungeonScenes.Count);
+            SceneManager.LoadScene(_dungeonScenes[sceneSelect]);
+        }
+        
 
 
     }
